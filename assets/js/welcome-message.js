57a0f05e252c394a584b47ccf8cd71e75e146551
German Bartoli <bartoligerman497@gmail.com>
2024-03-28 01:29:27 -0300

    Mensaje Bienvenida
    Deshabilitar elementos hijos al hacer clic en aceptar para que al pasar el mause el objeto no siga activo

--- assets/js/welcome-message.js
+++ assets/js/welcome-message.js
@@ -9,4 +9,12 @@ function aceptarBtnBienvenida(event) {
     document.getElementById("aSkills").classList.remove("disabled-link");
     document.getElementById("aPortfolio").classList.remove("disabled-link");
     document.getElementById("aContact").classList.remove("disabled-link");
+
+    var mensaje = document.getElementById('mensaje');
+    var elementosHijosDivMensaje = mensaje.querySelectorAll('*'); // Seleccionar todos los elementos hijos
+
+    elementosHijosDivMensaje.forEach(function (elemento) {
+        elemento.style.pointerEvents = 'none'; // Deshabilitar elementos hijos
+    });
+
 }
